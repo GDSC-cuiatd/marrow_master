74fbcf60554e733955d395a08525d4baa30461a8
Rizwan Shah <98143667+rizwanshah123@users.noreply.github.com>
2024-02-15 11:23:16 +0500

    'added'

---
+++ automated_bone_marrow_cell_classification_system/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:automated_bone_marrow_cell_classification_system/view/home_screen/home_main_screen.dart';
+import 'package:automated_bone_marrow_cell_classification_system/view/home_screen/result_screen/result_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'firebase_options.dart';
+import 'package:sizer/sizer.dart';
+import 'package:google_fonts/google_fonts.dart';
+import './view/on_boarding_screen/splash_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(builder: (context, orientation, deviceType) {
+      return GetMaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          useMaterial3: true,
+          textTheme: GoogleFonts.poppinsTextTheme(),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const SplashScreen(),
+      );
+    });
+  }
+}
